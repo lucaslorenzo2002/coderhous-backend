b23932d515e741234efa4ee06808a941940b37dd
lucas lorenzo <luquitas02@gmail.com>
2022-11-07 19:52:14 -0300

    ejercicios 2 y 3

--- ejercicio2/index2.js
+++ ejercicio2/index2.js
@@ -1,97 +1,99 @@
-const fs = require('fs')
+const fs = require('fs');
 
 class Contenedor {
     constructor(file){
         this.file = file
-       }
+    }
 
-       
-       //ESTA ES LA FUNCION QUE HICE PARA QUE LE AGREGUE EL ID
-       /* static async save(obj){
-           let productos = this.getAll() 
-           try{
-
-            let newId;
-            productos.length = 0 ? newId = 1 : newId = productos[productos.length-1].id + 1
-            let newObj = {...obj, id: newId}
-            productos.push(newObj)
-            await fs.promises.writeFile(archivo.file, JSON.stringify(productos, null, 2))
-          // return newObj.id;
-    } catch(err){
-            throw new Error('hubo un error: ' + err)
-        }
-       } */
-
-       //ESTA FUNCION LA HAGO PARA QUE SOLAMENTE ME CARGUE EL OBJETO
-       static async save(obj){
+    
+    static async writeFile(data){
         try{
-            await fs.promises.writeFile(archivo.file, JSON.stringify(obj, null, 2));
+            await fs.promises.writeFile(archivo.file, JSON.stringify(data, null, 2));
             console.log('producto agregado');
         }catch(err){
             throw new Error('hubo un error: ' + err)
         }
-       }
+    }
 
-       static async getAll (){
+     static async getAll (){
         try{
             let productos =  await fs.promises.readFile(archivo.file, 'utf-8');
-            return(JSON.parse(productos)); 
-            
-        } catch(err){
-            throw new Error('FileNotFoundError:' + err + 'No such file or directory')
+            return JSON.parse(productos); 
+            } catch(err){
+                if(err.message.includes('no such file or directory')) return [];
+                console.log('error: ' + err);
         }
-       }
-
-       static async getById (id){
-           let productos = await this.getAll()
+    }
+    
+    static async save(obj){
+        let productos = await this.getAll();
         try{
-           const productoID = productos.find(producto => producto.id === id)
+            let newId;
+            productos.length === 0 ? newId = 1 : newId = productos[productos.length - 1].id + 1
+            let newObj = {id: newId, ...obj}
+            productos.push(newObj)
+            await Contenedor.writeFile(productos)
+            return newObj.id;
+    } catch(err){
+            throw new Error('hubo un error: ' + err)
+        }  
+    } 
+
+    static async getById (id){
+        let productos = await this.getAll()
+        try{
+        const productoID = productos.find(producto => producto.id === id)
             id ? console.log( productoID) : console.log(null); 
         }catch(err){
-
+            throw new Error('hubo un error: ' + err)
         }
-       }
+    }
 
-       static async deleteAll (){
+    static async deleteAll (){
         try {
             await fs.promises.writeFile(archivo.file, [])
         } catch(err){
             throw new Error('hubo un error: ' + err)
         }
-       }
+    }
 
-       static async deleteById (id){
-           let productos = await this.getAll()
+    static async deleteById (id){
+        let productos = await this.getAll()
         try{
             productos = productos.filter(producto => producto.id !== id)
             await fs.promises.writeFile(archivo.file, JSON.stringify(productos, null, 2))
         }catch(err){
             throw new Error('hubo un error: ' + err)
         }
-       }
+    }
     }
 
+    const archivo = new Contenedor('./productos.txt')
 
+    const test = async () => {
+        let saveObj = await Contenedor.save(
+            {
+                producto: "fanta",
+                tamano: "3 litros",
+                stock: 20,
+                precio: 500,
+                thumbail: "https://http2.mlstatic.com/D_NQ_NP_632382-MLA49973524416_052022-O.webp"
+            }
+        )
+  
+//await Contenedor.deleteAll();
+//await Contenedor.deleteById();
+//let getById = await Contenedor.getById();
+let getAll = await Contenedor.getAll();
 
-const archivo = new Contenedor('./productos.txt')    
- Contenedor.save([{
-
-    producto: "coca-cola",
-    tamano: "2,25 litros",
-    stock: 25,
-    precio: 475,
-    thumbail: "https://http2.mlstatic.com/D_NQ_NP_632382-MLA49973524416_052022-O.webp"
-},{
-    producto: "pepsi",
-    tamano: "2 litros",
-    stock: 40,
-    precio: 400,
-    thumbail: "https://d3ugyf2ht6aenh.cloudfront.net/stores/001/219/229/products/40-51b4d2d07bc680935861f085d186fcdf1-ce8ed1c3777262d54916215172296748-50-0.jpg"
+//console.log(getById);
+console.log(getAll);
+console.log(saveObj);  
 }
-])  
+test()
 
-//Contenedor.deleteById()
-//Contenedor.getById() 
-//Contenedor.deleteAll()
+
+
+
 
 
---
+++ ejercicio3/index3.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const Contenedor = require('./contenedor')
+
+
+const app = express()
+const RUTA_1 = '/productos'
+const RUTA_2 = '/productorandom'
+const PORT = 8080
+
+
+    const productosContenedor = new Contenedor('productos.txt')
+
+    const randomNumber = (min, max) => {
+        return Math.floor(Math.random() * (max - min) + min)
+    }
+
+     app.get(RUTA_1, async (req, res) => {
+        const productos = await productosContenedor.getAll()
+        res.send(productos)
+    })  
+
+    
+
+     app.get(RUTA_2, async (req, res) => {
+        const productos = await productosContenedor.getAll()
+        const productoRANDOM = productos.find((producto) => producto.id === randomNumber(1, 3))
+        res.send(productoRANDOM)
+    })  
+
+    
+    const server = app.listen(process.env.PORT || PORT, () => {
+        console.log(` server listening on PORT: ${PORT}`);
+    })
+
+    server.on('error', err => console.log(err)) 
+
